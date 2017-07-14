9023dd999f8c8c8abf5835a96a9bcab2ec586db2
laheran7 <seserges2@gmail.com>
2017-07-14 13:15:57 +0000

    Old fragment_resto is deleted. Location_fragment is working with getter and setter attribut.

--- app/src/main/java/mivadounou/projet2/uut/ucao/mivadounou/fragments/LocationFragment.java
+++ app/src/main/java/mivadounou/projet2/uut/ucao/mivadounou/fragments/LocationFragment.java
@@ -7,13 +7,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import mivadounou.projet2.uut.ucao.mivadounou.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
+    public GoogleMap mMap;
+    private double latitude = 6.200616199999999;
+    private double longitude = 1.1809822999999824;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 
     public LocationFragment() {
         // Required empty public constructor
@@ -24,7 +51,19 @@ public class LocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View v = inflater.inflate(R.layout.fragment_location, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.main_branch_map);
+        mapFragment.getMapAsync(this);
+        return v;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        LatLng UCA = new LatLng(getLatitude(), getLongitude());
+        mMap.addMarker(new MarkerOptions().position(UCA).title("YOUR TITLE")).showInfoWindow();
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(UCA,17));
+    }
 }
